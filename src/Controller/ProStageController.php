2bcc66dd9a6a56fec173ccbb47fa325709daaa13
Romain Salha <rsalha@iutbayonne.univ-pau.fr>
2020-12-01 16:30:06 +0100

    mise en place des pages entreprises accueil formations et stages

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -5,16 +5,48 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class ProStageController extends AbstractController
 {
     /**
-     * @Route("/prostage/accueil", name="prostage")
+     * @Route("/accueil", name="prostage")
      */
     public function index(): Response
     {
         return $this->render('pro_stage/index.html.twig', [
             'controller_name' => 'ProStageController',
         ]);
+    }
+    /**
+     * @Route("/entreprises", name="entreprises")
+     */
+    public function entreprises()
+    {
+      return $this->render('pro_stage/entreprises.html.twig', [
+          'controller_name' => 'ProStageController',
+      ]);
+    }
+    /**
+     * @Route("/formations", name="formations")
+     */
+    public function formations()
+    {
+
+      return $this->render('pro_stage/formations.html.twig', [
+          'controller_name' => 'ProStageController',
+
+      ]);
+    }
+    /**
+     * @Route("/stages/{id}", name="stage")
+     */
+    public function stages($id)
+    {
+
+      return $this->render('pro_stage/stages.html.twig', [
+          'controller_name' => 'ProStageController',
+          'id' => $id
+      ]);
     }
 }
